6d91fc15f6e3387cb1ec834fce50e3f4f61199c0
Abhishek Anand <anandabhishek01555@gmail.com>
2020-08-02 11:04:30 +0530

    Update Codevita pattern.py

--- DAY 49/Codevita pattern.py
+++ DAY 49/Codevita pattern.py
@@ -1,4 +1,31 @@
 t=int(input())
-for i in range(n):
+for _ in range(t):
+    n=int(input())
+    cnt=10
+    a=[]
+    for i in range(n):
+        x=[]
+        for j in range(i):
+            x.append("**")
+        for j in range(i,n):
+            x.append(str(cnt))
+            cnt+=10
+        a.append(x)
+    print(a)
+    for i in range(n-1,-1,-1):
+        x=[]
+        for j in range(i,n):
+            x.append(str(cnt))
+            cnt+=10
+        a[i].extend(x)
+    print("Case #"+str(_+1))
+    for i in a:
+        x=''.join(i)
+        print(x[:-1])
+
+
+
+
+
     
 
