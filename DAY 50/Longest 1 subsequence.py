9fc7438cbd4b226ed7626952ba90a316144acaeb
Abhishek Anand <anandabhishek01555@gmail.com>
2020-08-06 22:08:41 +0530

    Add files via upload

---
+++ DAY 50/Longest 1 subsequence.py
@@ -0,0 +1,31 @@
+def longestSubSeg(a, n, k): 
+  
+    cnt0 = 0
+    l = 0
+    max_len = 0; 
+  
+    # i decides current ending point 
+    for i in range(0, n): 
+        if a[i] == 0: 
+            cnt0 += 1
+  
+        # If there are more 0's move 
+        # left point for current 
+        # ending point. 
+        while (cnt0 > k): 
+            if a[l] == 0: 
+                cnt0 -= 1
+            l += 1
+          
+  
+        max_len = max(max_len, i - l + 1); 
+      
+  
+    return max_len 
+  
+# Driver code 
+a = [1, 0, 0, 1, 0, 1, 0, 1 ] 
+k = 2
+n = len(a) 
+print(longestSubSeg(a, n, k)) 
+  
