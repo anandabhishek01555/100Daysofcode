742d1af6dbda00701daf9cf4baad150feed11c6e
Abhishek Anand <anandabhishek01555@gmail.com>
2020-08-28 08:38:32 +0530

    Delete Prefix sum array.py

--- DAY-58/Prefix sum array.py
+++
@@ -1,3 +0,0 @@
-# Consider an array of size N with all initial values as 0. Perform given 'm' add operations from index 'a' to 'b' and evaluate highest element in array. 
-# An add operation adds 100 to all elements from index a to b (both inclusive)
-
